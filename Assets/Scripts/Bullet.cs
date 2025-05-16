c61e15c1974d2dc74736b4c8b01b33ee24b43273
KLMIL <kanoline@naver.com>
2025-05-17 03:41:06 +0900

    [feat] 플레이어 캐릭터 추가 및 사격 기능 추가. 우클릭시 BulletTime 시작 및 궤적 표시

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,46 @@
+﻿/**********************************************************
+ * Script Name: Bullet
+ * Author: 김우성
+ * Date Created: 2025-05-16
+ * Last Modified: 2025-05-16
+ * Description: 
+ * - 플레이어가 발사할 총알 프리펩
+ *********************************************************/
+
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float _speed = 10f;
+    Vector3 _targetPosition; // 마우스 커서 위치
+    bool _hasTarget = false;
+
+    // PlayerController에서 호출해서 목표 위치 설정
+    public void SetTarget(Vector3 target)
+    {
+        _targetPosition = target;
+        _targetPosition.z = 0; // 2D라서 0
+        _hasTarget = true;
+    }
+
+    private void Update()
+    {
+        if (!_hasTarget) return;
+
+        // 목표 지점으로 이동, 불릿타임 보정
+        float step = _speed * Time.deltaTime / Time.timeScale;
+        transform.position = Vector3.MoveTowards(transform.position, _targetPosition, step);
+
+        if (Vector3.Distance(transform.position, _targetPosition) < 0.01f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        /* 적과 충돌 처리 추가 */
+        //Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Players/PlayerController.cs
@@ -0,0 +1,105 @@
+﻿/**********************************************************
+ * Script Name: PlayerController
+ * Author: 김우성
+ * Date Created: 2025-05-16
+ * Last Modified: 2025-05-16
+ * Description: 
+ * - 캐릭터의 움직임 및 상호작용 기능
+ *********************************************************/
+
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Move")]
+    [SerializeField] float _moveSpeed = 5f;
+    Rigidbody2D _rb;
+    Vector2 _moveInput;
+
+    [Header("Shoot")]
+    [SerializeField] GameObject _bulletPrefab;
+    [SerializeField] float _bulletSpeed = 10f;
+    [SerializeField] Transform _firePoint;
+
+    [Header("BulletTime")]
+    [SerializeField] float _bulletTimeScale = 0.2f;
+    bool _isBulletTime = false;
+
+    [Header("BulletLine")]
+    LineRenderer _lineRenderer;
+
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        // 이동 입력
+        _moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+        // 플레이어 회전 -> 마우스 방향
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        Vector3 direction = (mousePos - transform.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        // 궤적 업데이트
+        UpdateTrajectory(mousePos);
+
+        // 좌클릭으로 사격
+        if (Input.GetMouseButtonDown(0) && !_isBulletTime)
+        {
+            Shoot(mousePos);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            _isBulletTime = true;
+            Time.timeScale = _bulletTimeScale;
+            Time.fixedDeltaTime = 0.02f * Time.timeScale; // 물리 업데이트 보정
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            _isBulletTime = false;
+            Time.timeScale = 1f;
+            Time.fixedDeltaTime = 0.02f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.linearVelocity = _moveInput * _moveSpeed;
+    }
+
+    private void Shoot(Vector3 targetPos)
+    {
+        if (!_bulletPrefab || !_firePoint)
+        {
+            Debug.LogError("prefab or firepoint is not assigned");
+            return;
+        }
+        GameObject bullet = Instantiate(_bulletPrefab, _firePoint.position, _firePoint.rotation);
+        Bullet bulletScript = bullet.GetComponent<Bullet>();
+        bulletScript.SetTarget(targetPos);
+    }
+
+    private void UpdateTrajectory(Vector3 mousePos)
+    {
+        // bullettime 중에만 궤적 활성화
+        if (_firePoint && _isBulletTime)
+        {
+            _lineRenderer.enabled = true;
+            _lineRenderer.SetPosition(0, _firePoint.position);
+            _lineRenderer.SetPosition(1, mousePos);
+        }
+        else
+        {
+            _lineRenderer.enabled = false;
+        }
+    }
+}
